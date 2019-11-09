d8f47f7d4ae4e16c50a80f6f527d37e4d1b5b554
Rai Alexander <rai-alexander@users.noreply.github.com>
2019-11-09 14:40:51 +0300

    back up

---
+++ src/BundleAdjustment.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lenovo on 11/8/2019.
+//
+
+#include "BundleAdjustment.h"
---
+++ src/BundleAdjustment.h
@@ -0,0 +1,14 @@
+//
+// Created by Lenovo on 11/8/2019.
+//
+
+#ifndef SFM_3D_RECONSTRUCTION_BUNDLEADJUSTMENT_H
+#define SFM_3D_RECONSTRUCTION_BUNDLEADJUSTMENT_H
+
+
+class BundleAdjustment {
+
+};
+
+
+#endif //SFM_3D_RECONSTRUCTION_BUNDLEADJUSTMENT_H
